86df08826477c15953d4a494ebc992bc8c7383f5
Alex <alexander.sacht@ik-t.de>
2023-03-13 08:56:36 +0100

    cleaned up comments

--- src/plugins/gis_nebenbestimmungen/gisnebenbestimmungen.h
+++ src/plugins/gis_nebenbestimmungen/gisnebenbestimmungen.h
@@ -1,21 +1,4 @@
 /***************************************************************************
-    gisnebenbestimmungen.h
-    -------------------
-    begin                : Jan 21, 2004
-    copyright            : (C) 2004 by Tim Sutton
-    email                : tim@linfiniti.com
-
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-/***************************************************************************
  *   QGIS Programming conventions:
  *
  *   mVariableName - a class level member variable
--- src/plugins/gis_nebenbestimmungen/gisnebenbestimmungengui.cpp
+++ src/plugins/gis_nebenbestimmungen/gisnebenbestimmungengui.cpp
@@ -1,14 +1,4 @@
-/***************************************************************************
- *   Copyright (C) 2003 by Tim Sutton                                      *
- *   tim@linfiniti.com                                                     *
- *                                                                         *
- *   This is a plugin generated from the QGIS plugin template              *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+
 #include "gisnebenbestimmungengui.h"
 #include "qgshelp.h"
 
